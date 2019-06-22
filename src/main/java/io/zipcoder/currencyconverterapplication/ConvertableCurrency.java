9e83faff616d2655fa68da216e512d72e7783fde
rfrieger <rfrieger@gmail.com>
2019-06-22 14:58:42 -0400

    complete

--- src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
@@ -3,7 +3,7 @@ package io.zipcoder.currencyconverterapplication;
 import java.lang.reflect.Type;
 
 public interface ConvertableCurrency {
-        default Double convert(CurrencyType currencyType) {
+        public default Double convert(CurrencyType currencyType) {
             return currencyType.getRate()/getCurrencyType().getRate();
         }
 
